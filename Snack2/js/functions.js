c4d4238079061cce47fc2d0c090d349688aeedac
DanieleBaggiano <151195732+DanieleBaggiano@users.noreply.github.com>
2024-04-05 12:42:34 +0200

    inizio snack2

---
+++ Snack2/js/functions.js
@@ -0,0 +1,6 @@
+function lengthWords(userWord1, userWord2) {
+    let result;
+    if (userWord1 === userWord2) {
+        result = true;
+    } else
+}
---
+++ Snack2/js/script.js
@@ -0,0 +1,11 @@
+// Esercizio 2
+// Dare la possibilità di inserire due parole.
+// Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
+// Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+const userWord1 = prompt("Inserisci una parola");
+console.log(userWord1);
+
+const userWord2 = prompt("Inserisci un'altra parola");
+console.log(userWord2);
+
